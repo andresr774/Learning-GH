01e1fb22acb4debbea631e1323d5e6b2bd43ecb6
Andres <andresr774@gmail.com>
2021-04-05 11:25:59 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Andres camilo Raigoza misas on 5/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
